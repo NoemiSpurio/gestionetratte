a7b25e225ea7f32927ab90315ffb134d77e475b9
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-25 16:12:12 +0100

    implementazione repository di tratta

---
+++ src/main/java/it/prova/gestionetratte/repository/tratta/CustomTrattaRepositoryImpl.java
@@ -0,0 +1,5 @@
+package it.prova.gestionetratte.repository.tratta;
+
+public class CustomTrattaRepositoryImpl implements CustomTrattaRepository {
+
+}
---
+++ src/main/java/it/prova/gestionetratte/repository/tratta/TrattaRepository.java
@@ -0,0 +1,17 @@
+package it.prova.gestionetratte.repository.tratta;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.gestionetratte.model.Tratta;
+
+public interface TrattaRepository extends CrudRepository<Tratta, Long>, CustomTrattaRepository {
+
+	@Query("from Tratta t join fetch t.airbus where t.id = ?1")
+	Tratta findByIdEager(Long id);
+	
+	@Query("select t from Tratta t join fetch t.airbus")
+	List<Tratta> findAllEager();
+}
