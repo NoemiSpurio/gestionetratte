8afef4d62633105848dfc4794b92dfd77144f0fb
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-27 11:40:21 +0100

    controller findbyid airbus

--- src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
+++ src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.gestionetratte.dto.AirbusDTO;
+import it.prova.gestionetratte.model.Airbus;
 import it.prova.gestionetratte.service.airbus.AirbusService;
+import it.prova.gestionetratte.web.api.exception.AirbusNotFoundException;
 
 @RestController
 @RequestMapping("api/airbus")
@@ -20,5 +23,15 @@ public class AirbusController {
 	@GetMapping
 	public List<AirbusDTO> listAll(){
 		return AirbusDTO.createAirbusDTOListFromModelList(airbusService.listAllEager(), true);
+	}
+	
+	@GetMapping("/{id}")
+	public AirbusDTO findById(@PathVariable(value = "id", required = true) long id) {
+		Airbus airbus = airbusService.caricaSingoloElementoEager(id);
+
+		if (airbus == null)
+			throw new AirbusNotFoundException("Airbus not found con id: " + id);
+
+		return AirbusDTO.buildAirbusDTOFromModel(airbus, true);
 	}
 }
