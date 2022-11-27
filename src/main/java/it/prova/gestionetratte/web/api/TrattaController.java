4c090ab63f1d2178f200e1bb326358970affe811
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-27 12:22:46 +0100

    implementazione inserimento tratta

--- src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
+++ src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
@@ -2,16 +2,24 @@ package it.prova.gestionetratte.web.api;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.gestionetratte.dto.TrattaDTO;
+import it.prova.gestionetratte.model.Airbus;
 import it.prova.gestionetratte.model.Tratta;
+import it.prova.gestionetratte.service.airbus.AirbusService;
 import it.prova.gestionetratte.service.tratta.TrattaService;
+import it.prova.gestionetratte.web.api.exception.IdNotNullForInsertException;
 import it.prova.gestionetratte.web.api.exception.TrattaNotFoundException;
+import it.prova.gestionetratte.web.api.exception.TrattaValidationException;
 
 @RestController
 @RequestMapping("api/tratta")
@@ -20,11 +28,14 @@ public class TrattaController {
 	@Autowired
 	private TrattaService trattaService;
 	
+	@Autowired
+	private AirbusService airbusService;
+
 	@GetMapping
 	public List<TrattaDTO> getAll() {
 		return TrattaDTO.createTrattaDTOListFromModelList(trattaService.listAll(true), true);
 	}
-	
+
 	@GetMapping("/{id}")
 	public TrattaDTO findById(@PathVariable(value = "id", required = true) long id) {
 		Tratta tratta = trattaService.caricaSingoloElementoEager(id);
@@ -33,5 +44,25 @@ public class TrattaController {
 			throw new TrattaNotFoundException("Tratta not found con id: " + id);
 
 		return TrattaDTO.buildTrattaDTOFromModel(tratta, true);
+	}
+
+	@PostMapping
+	public TrattaDTO createNew(@Valid @RequestBody TrattaDTO trattaInput) {
+
+		if (trattaInput.getId() != null)
+			throw new IdNotNullForInsertException("Non è ammesso fornire un id per la creazione");
+
+		Airbus airbusPerValidazioni = airbusService.caricaSingoloElemento(trattaInput.getAirbus().getId());
+
+		if (trattaInput.getData().isBefore(airbusPerValidazioni.getDataInizioServizio()))
+			throw new TrattaValidationException(
+					"Non e' possibile inserire una tratta con data di partenza precedente a quella di inizio servizio dell airbus.");
+
+		if (trattaInput.getOraDecollo().isAfter(trattaInput.getOraAtterraggio()))
+			throw new TrattaValidationException(
+					"Non e' possibile inserire una tratta con orario di decollo successivo a quello di atterraggio.");
+
+		Tratta trattaInserita = trattaService.inserisciNuovo(trattaInput.buildTrattaModel());
+		return TrattaDTO.buildTrattaDTOFromModel(trattaInserita, true);
 	}
 }
