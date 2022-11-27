9d0dcba18cf34df388d4346dbfbc1f2366e73ee4
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-27 11:58:33 +0100

    implementazione update airbus

--- src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
+++ src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -57,5 +58,21 @@ public class AirbusController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable(required = true) Long id) {
 		airbusService.rimuovi(id);
+	}
+	
+	@PutMapping("/{id}")
+	public AirbusDTO update(@Valid @RequestBody AirbusDTO airbusInput, @PathVariable(required = true) Long id) {
+		
+		Airbus airbus = airbusService.caricaSingoloElementoEager(id);
+		
+		if (airbus == null) {
+			throw new AirbusNotFoundException("Airbus not found con id: " + id);
+		}
+		
+		airbusInput.setId(id);
+		
+		Airbus airbusAggiornato = airbusService.aggiorna(airbusInput.buildAirbusModel());
+		
+		return AirbusDTO.buildAirbusDTOFromModel(airbusAggiornato, false);
 	}
 }
